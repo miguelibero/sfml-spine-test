6bb30daad12af252312578b3de5210b1f3bec1f1
Miguel Ibero <miguel.ibero.carreras@gmail.com>
2013-02-24 22:54:49 +0100

    project compiles

---
+++ proj.linux/CMakeFiles/spine.dir/DependInfo.cmake
@@ -0,0 +1,40 @@
+# The set of languages for which implicit dependencies are needed:
+SET(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+SET(CMAKE_DEPENDS_CHECK_CXX
+  "/home/mibero/Projects/sfml-spine-test/lib/spine/spine-cpp/src/json/json_reader.cpp" "/home/mibero/Projects/sfml-spine-test/proj.linux/CMakeFiles/spine.dir/home/mibero/Projects/sfml-spine-test/lib/spine/spine-cpp/src/json/json_reader.cpp.o"
+  "/home/mibero/Projects/sfml-spine-test/lib/spine/spine-cpp/src/json/json_value.cpp" "/home/mibero/Projects/sfml-spine-test/proj.linux/CMakeFiles/spine.dir/home/mibero/Projects/sfml-spine-test/lib/spine/spine-cpp/src/json/json_value.cpp.o"
+  "/home/mibero/Projects/sfml-spine-test/lib/spine/spine-cpp/src/json/json_writer.cpp" "/home/mibero/Projects/sfml-spine-test/proj.linux/CMakeFiles/spine.dir/home/mibero/Projects/sfml-spine-test/lib/spine/spine-cpp/src/json/json_writer.cpp.o"
+  "/home/mibero/Projects/sfml-spine-test/lib/spine/spine-cpp/src/spine-sfml/Atlas.cpp" "/home/mibero/Projects/sfml-spine-test/proj.linux/CMakeFiles/spine.dir/home/mibero/Projects/sfml-spine-test/lib/spine/spine-cpp/src/spine-sfml/Atlas.cpp.o"
+  "/home/mibero/Projects/sfml-spine-test/lib/spine/spine-cpp/src/spine-sfml/AtlasAttachmentLoader.cpp" "/home/mibero/Projects/sfml-spine-test/proj.linux/CMakeFiles/spine.dir/home/mibero/Projects/sfml-spine-test/lib/spine/spine-cpp/src/spine-sfml/AtlasAttachmentLoader.cpp.o"
+  "/home/mibero/Projects/sfml-spine-test/lib/spine/spine-cpp/src/spine-sfml/RegionAttachment.cpp" "/home/mibero/Projects/sfml-spine-test/proj.linux/CMakeFiles/spine.dir/home/mibero/Projects/sfml-spine-test/lib/spine/spine-cpp/src/spine-sfml/RegionAttachment.cpp.o"
+  "/home/mibero/Projects/sfml-spine-test/lib/spine/spine-cpp/src/spine-sfml/Skeleton.cpp" "/home/mibero/Projects/sfml-spine-test/proj.linux/CMakeFiles/spine.dir/home/mibero/Projects/sfml-spine-test/lib/spine/spine-cpp/src/spine-sfml/Skeleton.cpp.o"
+  "/home/mibero/Projects/sfml-spine-test/lib/spine/spine-cpp/src/spine-sfml/SkeletonJson.cpp" "/home/mibero/Projects/sfml-spine-test/proj.linux/CMakeFiles/spine.dir/home/mibero/Projects/sfml-spine-test/lib/spine/spine-cpp/src/spine-sfml/SkeletonJson.cpp.o"
+  "/home/mibero/Projects/sfml-spine-test/lib/spine/spine-cpp/src/spine/Animation.cpp" "/home/mibero/Projects/sfml-spine-test/proj.linux/CMakeFiles/spine.dir/home/mibero/Projects/sfml-spine-test/lib/spine/spine-cpp/src/spine/Animation.cpp.o"
+  "/home/mibero/Projects/sfml-spine-test/lib/spine/spine-cpp/src/spine/BaseAtlas.cpp" "/home/mibero/Projects/sfml-spine-test/proj.linux/CMakeFiles/spine.dir/home/mibero/Projects/sfml-spine-test/lib/spine/spine-cpp/src/spine/BaseAtlas.cpp.o"
+  "/home/mibero/Projects/sfml-spine-test/lib/spine/spine-cpp/src/spine/BaseRegionAttachment.cpp" "/home/mibero/Projects/sfml-spine-test/proj.linux/CMakeFiles/spine.dir/home/mibero/Projects/sfml-spine-test/lib/spine/spine-cpp/src/spine/BaseRegionAttachment.cpp.o"
+  "/home/mibero/Projects/sfml-spine-test/lib/spine/spine-cpp/src/spine/BaseSkeleton.cpp" "/home/mibero/Projects/sfml-spine-test/proj.linux/CMakeFiles/spine.dir/home/mibero/Projects/sfml-spine-test/lib/spine/spine-cpp/src/spine/BaseSkeleton.cpp.o"
+  "/home/mibero/Projects/sfml-spine-test/lib/spine/spine-cpp/src/spine/BaseSkeletonJson.cpp" "/home/mibero/Projects/sfml-spine-test/proj.linux/CMakeFiles/spine.dir/home/mibero/Projects/sfml-spine-test/lib/spine/spine-cpp/src/spine/BaseSkeletonJson.cpp.o"
+  "/home/mibero/Projects/sfml-spine-test/lib/spine/spine-cpp/src/spine/Bone.cpp" "/home/mibero/Projects/sfml-spine-test/proj.linux/CMakeFiles/spine.dir/home/mibero/Projects/sfml-spine-test/lib/spine/spine-cpp/src/spine/Bone.cpp.o"
+  "/home/mibero/Projects/sfml-spine-test/lib/spine/spine-cpp/src/spine/SkeletonData.cpp" "/home/mibero/Projects/sfml-spine-test/proj.linux/CMakeFiles/spine.dir/home/mibero/Projects/sfml-spine-test/lib/spine/spine-cpp/src/spine/SkeletonData.cpp.o"
+  "/home/mibero/Projects/sfml-spine-test/lib/spine/spine-cpp/src/spine/Skin.cpp" "/home/mibero/Projects/sfml-spine-test/proj.linux/CMakeFiles/spine.dir/home/mibero/Projects/sfml-spine-test/lib/spine/spine-cpp/src/spine/Skin.cpp.o"
+  "/home/mibero/Projects/sfml-spine-test/lib/spine/spine-cpp/src/spine/Slot.cpp" "/home/mibero/Projects/sfml-spine-test/proj.linux/CMakeFiles/spine.dir/home/mibero/Projects/sfml-spine-test/lib/spine/spine-cpp/src/spine/Slot.cpp.o"
+  )
+SET(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Targets to which this target links.
+SET(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# The include file search paths:
+SET(CMAKE_C_TARGET_INCLUDE_PATH
+  "src"
+  "/home/mibero/Projects/sfml-spine-test/lib/sfml/include"
+  "../lib/jsoncpp/src"
+  "../lib/spine/spine-cpp/includes"
+  )
+SET(CMAKE_CXX_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
+SET(CMAKE_Fortran_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
+SET(CMAKE_ASM_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
---
+++ proj.linux/CMakeFiles/spine.dir/cmake_clean.cmake
@@ -0,0 +1,26 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/spine.dir/home/mibero/Projects/sfml-spine-test/lib/spine/spine-cpp/src/json/json_reader.cpp.o"
+  "CMakeFiles/spine.dir/home/mibero/Projects/sfml-spine-test/lib/spine/spine-cpp/src/json/json_value.cpp.o"
+  "CMakeFiles/spine.dir/home/mibero/Projects/sfml-spine-test/lib/spine/spine-cpp/src/json/json_writer.cpp.o"
+  "CMakeFiles/spine.dir/home/mibero/Projects/sfml-spine-test/lib/spine/spine-cpp/src/spine/Animation.cpp.o"
+  "CMakeFiles/spine.dir/home/mibero/Projects/sfml-spine-test/lib/spine/spine-cpp/src/spine/BaseAtlas.cpp.o"
+  "CMakeFiles/spine.dir/home/mibero/Projects/sfml-spine-test/lib/spine/spine-cpp/src/spine/BaseRegionAttachment.cpp.o"
+  "CMakeFiles/spine.dir/home/mibero/Projects/sfml-spine-test/lib/spine/spine-cpp/src/spine/BaseSkeleton.cpp.o"
+  "CMakeFiles/spine.dir/home/mibero/Projects/sfml-spine-test/lib/spine/spine-cpp/src/spine/BaseSkeletonJson.cpp.o"
+  "CMakeFiles/spine.dir/home/mibero/Projects/sfml-spine-test/lib/spine/spine-cpp/src/spine/Bone.cpp.o"
+  "CMakeFiles/spine.dir/home/mibero/Projects/sfml-spine-test/lib/spine/spine-cpp/src/spine/SkeletonData.cpp.o"
+  "CMakeFiles/spine.dir/home/mibero/Projects/sfml-spine-test/lib/spine/spine-cpp/src/spine/Skin.cpp.o"
+  "CMakeFiles/spine.dir/home/mibero/Projects/sfml-spine-test/lib/spine/spine-cpp/src/spine/Slot.cpp.o"
+  "CMakeFiles/spine.dir/home/mibero/Projects/sfml-spine-test/lib/spine/spine-cpp/src/spine-sfml/Atlas.cpp.o"
+  "CMakeFiles/spine.dir/home/mibero/Projects/sfml-spine-test/lib/spine/spine-cpp/src/spine-sfml/AtlasAttachmentLoader.cpp.o"
+  "CMakeFiles/spine.dir/home/mibero/Projects/sfml-spine-test/lib/spine/spine-cpp/src/spine-sfml/RegionAttachment.cpp.o"
+  "CMakeFiles/spine.dir/home/mibero/Projects/sfml-spine-test/lib/spine/spine-cpp/src/spine-sfml/Skeleton.cpp.o"
+  "CMakeFiles/spine.dir/home/mibero/Projects/sfml-spine-test/lib/spine/spine-cpp/src/spine-sfml/SkeletonJson.cpp.o"
+  "bin/libspine.pdb"
+  "bin/libspine.a"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang CXX)
+  INCLUDE(CMakeFiles/spine.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
